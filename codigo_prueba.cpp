1d1659f080b13a9a1e0e32bb0b8f3a0cd7adbe5f
manzanara <e.manzanara@go.ugr.es>
2023-03-13 19:14:57 +0100

    he creado un codigo

---
+++ codigo_prueba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std ;
+
+int main (void){
+    int num ;
+
+    cout << "Introducir número para que salga por pantalla: " ;
+    cin >> num ;
+
+    cout << "El num es: " << num << endl ;
+
+    return 0 ;
+}
